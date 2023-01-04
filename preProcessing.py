7e99abe21274774e9269ebb28281aaa7e979432e
wjk1011 <86654135+wjk1011@users.noreply.github.com>
2023-01-04 16:29:12 +0900

    Update preProcessing.py

--- preProcessing.py
+++ preProcessing.py
@@ -3,7 +3,7 @@ import numpy as np
 import data_load as data_load
 
 
-def preprocessingData(df, _portion):
+def preprocessingData(config):
     def data_split(data, _portion: float):
         if _portion == 0:
             train_data = copy.deepcopy(data)
@@ -18,20 +18,22 @@ def preprocessingData(df, _portion):
 
         return np.array(train_data), np.array(test_data)
 
-    def preprocessing(df, _portion):
-        decision = sorted(list(set(map(lambda x: x[-1], df))))
-        for _index, _decision in enumerate(decision):
-            for _data in df:
-                if _data[-1] == _decision:
-                    _data[-1] = str(_index)
+    def preprocessing(config):
+        df, target_names, _portion = config['data'].to_numpy(), config['target_names'], config['split_portion']
+        for _index, _target_name in enumerate(target_names):
+            # for i in df:
+            #     if str(i[-1]) == _target_name:
+            #         i[-1] = str(_index)
+            df = np.where(df == _target_name, str(_index), df)
 
         train_data_value, test_data_value = data_split(df, _portion)
         train_data = [data_load.Data(attribute_name, train_data_value[i])
                       for i in range(train_data_value.shape[0])]
+        test_data = [data_load.Data(attribute_name, test_data_value[i])
+                     for i in range(test_data_value.shape[0])]
         for i in range(len(train_data)):
             train_data[i].id = i
-        test_data = [data_load.Data(attribute_name, test_data_value[i]) for i in
-                     range(test_data_value.shape[0])]
+
         for i in range(len(test_data)):
             test_data[i].id = i
 
@@ -41,13 +43,14 @@ def preprocessingData(df, _portion):
             attribute.append(_name)
         for _index in range(len(attribute)):
             attribute[_index].name = attribute_name[_index]
+            attribute[_index].unit = data_load.get_steel_unit(attribute[_index].name)
             data_load.attribute_set(attribute[_index], train_data)
             data_load.attribute_set(attribute[_index], test_data)
 
         return train_data, test_data, attribute
 
 
-    attribute_name = [i.replace(' ', '') for i in df]
-    train_data, test_data, attribute = preprocessing(df.to_numpy(), _portion)
+    attribute_name = [i.replace(' ', '') for i in config['data']]
+    train_data, test_data, attribute = preprocessing(config)
 
     return train_data, test_data, attribute
